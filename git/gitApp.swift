5aa04281fed305727b5dbfc56b7069604961f67d
Burhan Raza <burhanraza98@gmail.com>
2024-11-04 15:19:38 +0100

    Initial Commit

---
+++ git/gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitApp.swift
+//  git
+//
+//  Created by Burhan Raza on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
